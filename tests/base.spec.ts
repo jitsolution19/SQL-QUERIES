3066297d878f3ebd995b5aa7dffd8074af0b720f
jitendra <jitsolution19@gmail.com>
2025-04-06 15:44:06 +0530

    updated the code

---
+++ tests/base.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+let browser;
+let context;
+let page;
+
+
+test.beforeAll(async () => {
+    const playwright = require('playwright');
+    browser = await playwright.chromium.launch();
+    context = await browser.newContext();
+    page = await context.newPage(); 
+  });
+  
+  test.afterAll(async () => {
+    await page.close();
+    await context.close();
+    await browser.close();
+  });
+
+export { browser, context, page };
--- tests/moneyControl.spec.ts
+++ tests/moneyControl.spec.ts
@@ -5,3 +5,37 @@ test('has title', async ({ page }) => {
   
   await expect(page).toHaveTitle("Yahoo Finance - Stock Market Live, Quotes, Business & Finance News");
 });
+
+test('chatgpt has title', async ({ page }) => {
+  await page.goto('https://chatgpt.com');
+  
+  await expect(page).toHaveTitle("ChatGPT");
+});
+
+const data={
+  "expectedTitle": ["Business News Today: Stock and Share Market News"]
+}
+
+test('moneycontrol has title', async () => {
+  const playwright = require('playwright');
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.moneycontrol.com/');
+  try {
+    const linktext=await page.locator("span a[title='Moneycontrol']");
+    await linktext.click();
+  } catch (error) {
+    error.message =('link text not found');
+  }
+  try {
+    await page.locator('#wzrk-cancel').click();
+  } catch (error) {
+    error.message ='Notifications not available';
+  }
+  await expect(page).toHaveTitle(data.expectedTitle[0]);
+  await page.screenshot({ path: 'screenshot.png' });
+  await context.close();
+  await page.close();
+  await browser.close();
+});
