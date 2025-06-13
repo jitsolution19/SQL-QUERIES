7245a79e2f5c554037908ab199c5f1cba13b1aeb
jitendra <jitsolution19@gmail.com>
2025-06-14 03:29:54 +0530

    Edge Browser Setup

--- tests/amazonPom.spec.ts
+++ tests/amazonPom.spec.ts
@@ -10,3 +10,8 @@ test('Getting Dino Lunch box Info',async({amazonPage})=>{
   const appurl ='https://www.amazon.in/s?k=dino+lunch+box&crid=1LJX1PAHGHB74&sprefix=dinolunch+box%2Caps%2C256&ref=nb_sb_noss';
   await amazonPage.getCatLunchBoxInfo(appurl);
 });
+
+test('Getting Color Pencil for Kids Info',async({amazonPage})=>{
+  const appurl ='https://www.amazon.in/s?k=color+pencils+for+kids&crid=MOTI31B4YXXI&sprefix=color+%2Caps%2C341&ref=nb_sb_ss_ts-doa-p_1_6';
+  await amazonPage.getCatLunchBoxInfo(appurl);
+});
